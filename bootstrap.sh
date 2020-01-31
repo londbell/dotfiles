f9b60e0f1798855ff46fd8136b7f66c328767dfc
冯文杰 <fengwenjie@meituan.com>
2020-01-31 21:06:06 +0800

    feat(global): 加入了etc和bin的一部分配置

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,37 @@ set -x
 
 ETC=~/.local/etc
 BIN=~/.local/bin
+DOTFILES_GIT_URL="https://github.com/londbell/dotfiles.git"
+
+
 mkdir -p $ETC
 mkdir -p $BIN
 
-cd ~/.local/
-if [-d U]
+updateDotfiles() {
+    cd ~/.local/
+    if [ -d dotfiles ]; then
+        cd dotfiles
+        git pull
+    else
+        # 使用变量时前面加上$取值
+        git clone $DOTFILES_GIT_URL
+        cd dotfiles
+    fi
 
+    cp -rf etc/* $ETC/
+    cp -rf bin/* $BIN/
+    cp bootstrap.sh $BIN/
+}
+
+configGit() {
+    cd ~/.local/
+    if [ -f etc/git-config.sh ]; then
+        sh git-config.sh
+    else
+        echo "git.sh don't exist;"
+    fi
+}
+
+# shell函数执行不需要()
+updateDotfiles
+configGit
