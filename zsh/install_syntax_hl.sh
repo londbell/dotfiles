1dd1e19b4c67d44ea775aeda8fba78446d49cc61
Londbell <tmacfeng@yeah.net>
2020-02-16 23:43:14 +0800

    Update install_syntax_hl.sh

--- zsh/install_syntax_hl.sh
+++ zsh/install_syntax_hl.sh
@@ -7,5 +7,5 @@ SYNHL_GIT_URL="https://github.com/zsh-users/zsh-syntax-highlighting.git"
 git clone ${SYNHL_GIT_URL} "${DOTFILES_ZSH}"
 
 # 移除可能存在的source信息
-sed -i "" "\:source ${DOTFILES_ZSH}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh:d" ${HOME}/.zshrc
+sed -i "\:source ${DOTFILES_ZSH}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh:d" ${HOME}/.zshrc
 echo "source ${DOTFILES_ZSH}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${HOME}/.zshrc
